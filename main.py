3a4dcb8f8594e618572c890755d7b6feba96de7e
Miguel Hernandez <migher74@outlook.com>
2024-10-02 21:49:59 -0600

    Adding the main label and the frame that contains it.

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from ttkbootstrap import Window
+from ttkbootstrap import Window, Frame, Label
 
 
 class Login(Window):
     def __init__(self):
-        super().__init__(title="Login")
+        super().__init__(title="Login", resizable=(False, False))
+
+        # Label Frame
+        self.lblFrame = Frame(self)
+        self.lblFrame.grid(row=0, column=0)
+
+        Label(self.lblFrame, text="Login",
+              font=("Arial", "20", "bold")).pack()
 
 
 if __name__ == ("__main__"):
