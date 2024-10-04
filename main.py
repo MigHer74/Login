eef6d5706747a298199a3d0f56f6c01be7f67635
Miguel Hernandez <migher74@outlook.com>
2024-10-03 22:05:52 -0600

    Adding an image and the frame that contains it.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from ttkbootstrap import Window, Frame, Label
+import tools as tls
 
 
 class Login(Window):
@@ -7,10 +8,20 @@ class Login(Window):
 
         # Label Frame
         self.lblFrame = Frame(self)
-        self.lblFrame.grid(row=0, column=0)
+        self.lblFrame.pack()
 
-        Label(self.lblFrame, text="Login",
-              font=("Arial", "20", "bold")).pack()
+        self.prueba1 = Label(self.lblFrame, text="Login",
+                             font=("Arial", "20", "bold"))
+        self.prueba1.pack()
+
+        # Label Image
+        self.imgFront = tls.imageItem("login.png", 250, 250)
+
+        self.imgFrame = Frame(self)
+        self.imgFrame.pack()
+
+        self.prueba = Label(self.imgFrame, image=self.imgFront)
+        self.prueba.pack()
 
 
 if __name__ == ("__main__"):
