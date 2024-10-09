ccd8c89c39047e5f205de55a25ea3651855c0214
Miguel Hernandez <migher74@outlook.com>
2024-10-09 15:43:30 -0600

    Adding the table (treeview) to show the list of users and their info.

--- users.py
+++ users.py
@@ -1,4 +1,4 @@
-from ttkbootstrap import Toplevel, Frame, Label, Entry
+from ttkbootstrap import Toplevel, Frame, Label, Entry, Treeview
 
 
 class UsersWindow(Toplevel):
@@ -19,14 +19,32 @@ class UsersWindow(Toplevel):
         self.entId = Entry(self.infoUser, width=10, state="disabled")
         self.entId.grid(row=1, column=0, padx=(15, 0), pady=(3, 0))
 
-        self.lblName = Label(self.infoUser, text="User Name")
-        self.lblName.grid(row=0, column=1,padx=(15, 0), pady=(15, 0))
+        self.lblName = Label(self.infoUser, text="Name")
+        self.lblName.grid(row=0, column=1, padx=(15, 0), pady=(15, 0))
 
         self.entName = Entry(self.infoUser, width=50, state="disabled")
         self.entName.grid(row=1, column=1, padx=(15, 0), pady=(3, 0))
 
         self.lblPassword = Label(self.infoUser, text="Password")
-        self.lblPassword.grid(row=0, column=2,padx=(15, 15), pady=(15, 0))
+        self.lblPassword.grid(row=0, column=2, padx=(15, 15), pady=(15, 0))
 
         self.entPassword = Entry(self.infoUser, width=30, state="disabled")
         self.entPassword.grid(row=1, column=2, padx=(15, 15), pady=(3, 0))
+
+        # Table Frame
+        self.tblFrame = Frame(self)
+        self.tblFrame.pack(padx=(15, 0), pady=(15, 0), anchor="w")
+
+        self.tblUser = Treeview(self.tblFrame, columns=(1, 2, 3),
+                                show="headings", height=15,
+                                selectmode="browse", bootstyle="info")
+
+        self.tblUser.heading(1, text="Id")
+        self.tblUser.heading(2, text="Name")
+        self.tblUser.heading(3, text="Password")
+
+        self.tblUser.column(1, width=50, anchor="center")
+        self.tblUser.column(2, width=150)
+        self.tblUser.column(3, width=150)
+
+        self.tblUser.pack()
